97065eecabfcc15d8021b85d0d695fdf1add5349
Nicolas Pollet <you@example.com>
2026-02-10 15:53:36 +0100

    ajout du skeleton

--- Exos_HTML/projetInstagram/index.js
+++ Exos_HTML/projetInstagram/index.js
@@ -1,2 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {
-const elements = document.querySelectorAll(".title, .card");
+document.addEventListener('DOMContentLoaded', () => {
+    const photos = document.querySelectorAll(".photo-container,.photo-container img,section,#photoprofil img,i,#logofleche,#menu,#cloche,#check,#titre h4,.photo-container,#following,#message,#mail,p,a,.ligne2 h4,.descriptions h4,li,#photoprofil");
+
+    photos.forEach(photo => {
+        photo.classList.add('skeleton');
+    });
+    // setTimeout(() => {
+    //     photos.forEach(photo => {
+    //         photo.classList.remove('skeleton');
+    //     });
+    // }, 3000);
+});
+
+
